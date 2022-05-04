d35f6fb3aa8217aaf3d65db2c0b19dc25711d48f
Nate <7nater.nelson@gmail.com>
2022-05-04 17:34:33 -0600

    Member Blurb

---
+++ src/components/reporting-member-profile/index.tsx
@@ -0,0 +1,78 @@
+import { ArrowDownward, ArrowDropDown, Edit } from "@mui/icons-material";
+import { Box, Button, Divider, IconButton, List, ListItem, Typography } from "@mui/material";
+
+export default function MemberSidebar(){
+    return (
+        <Box sx={{p:3}} display={"flex"} flexDirection="column">
+            <Box display="flex" flexDirection="row" justifyContent={"center"}>
+                <Typography variant={"h5"} align="center">Anthony Jarvis</Typography>
+                <IconButton>
+                    <Edit sx={{fontSize:12}}/>
+                </IconButton>
+            </Box>
+            <Typography variant={"caption"} align="center">Salt Lake City 2nd Stake</Typography>
+            <Typography variant={"caption"} align="center">Alpine 3rd Ward</Typography>
+            <Divider/>
+            <Box mt={1}>
+                <Button fullWidth={true} size="small" variant="outlined" sx={{gap:1}}>
+                    <Typography>
+                        Actions
+                    </Typography>
+                    <ArrowDropDown fontSize="small"/>
+                </Button>
+            </Box>
+            <Box display="flex" flexDirection="row" justifyContent={"center"} sx={{gap:1,mt:1}}>
+                <Typography variant="body2">STATUS</Typography>
+                <Typography variant="body2" color="white" sx={{bgcolor:'success.main', px:1}}>ACTIVE</Typography>
+            </Box>
+            <Box sx={{my: 2, p:1, borderTop:1, borderColor: 'grey.500', bgcolor:'grey.50'}}>
+                <Box display="flex" flexDirection="row" justifyContent={"center"}>
+                    <Typography variant="overline" align="center" sx={{textAlign:'center', width: '100%'}}>history</Typography>
+                    <Divider/>
+                </Box>
+                <List>
+                    <ListItem alignItems="flex-start" sx={{gap:1, flexDirection:'column'}}>
+                        <Typography sx={{color: 'grey.600', fontStyle:"italic", fontSize: '12px', lineHeight:1}}>
+                            Anthony received a new calling
+                        </Typography>
+                        <Typography sx={{color: 'grey.600', fontStyle:"italic", fontSize: '10px',lineHeight:0.5}}>
+                            @{new Date().toLocaleString()}
+                        </Typography>
+                    </ListItem>
+                    <ListItem alignItems="flex-start" sx={{gap:1, flexDirection:'column'}}>
+                        <Typography sx={{color: 'grey.600', fontStyle:"italic", fontSize: '12px', lineHeight:1}}>
+                            Anthony met with Elder's Quorum Leadership
+                        </Typography>
+                        <Typography sx={{color: 'grey.600', fontStyle:"italic", fontSize: '10px',lineHeight:0.5}}>
+                            @{new Date().toLocaleString()}
+                        </Typography>
+                    </ListItem>
+                    <ListItem alignItems="flex-start" sx={{gap:1, flexDirection:'column'}}>
+                        <Typography sx={{color: 'grey.600', fontStyle:"italic", fontWeight:'bold', fontSize: '12px', lineHeight:1}}>
+                            Anthony's status changed from unknown to Active
+                        </Typography>
+                        <Typography sx={{color: 'grey.600', fontStyle:"italic", fontSize: '10px',lineHeight:0.5}}>
+                            @{new Date().toLocaleString()}
+                        </Typography>
+                    </ListItem>
+                    <ListItem alignItems="flex-start" sx={{gap:1, flexDirection:'column'}}>
+                        <Typography sx={{color: 'grey.600', fontStyle:"italic", fontSize: '12px', lineHeight:1}}>
+                            Anthony met with Bishop
+                        </Typography>
+                        <Typography sx={{color: 'grey.600', fontStyle:"italic", fontSize: '10px',lineHeight:0.5}}>
+                            @{new Date().toLocaleString()}
+                        </Typography>
+                    </ListItem>
+                    <ListItem alignItems="flex-start" sx={{gap:1, flexDirection:'column'}}>
+                        <Typography sx={{color: 'grey.600', fontStyle:"italic", fontSize: '12px', lineHeight:1}}>
+                            Anthony's records were received
+                        </Typography>
+                        <Typography sx={{color: 'grey.600', fontStyle:"italic", fontSize: '10px',lineHeight:0.5}}>
+                            @{new Date().toLocaleString()}
+                        </Typography>
+                    </ListItem>
+                </List>
+            </Box>
+        </Box>
+    )
+}
--- src/views/reports/index.tsx
+++ src/views/reports/index.tsx
@@ -1,5 +1,6 @@
 import { Box, Paper } from "@mui/material";
 import WardList from "../../components/report-ward-list";
+import MemberSidebar from "../../components/reporting-member-profile";
 import DemoReportingTable from "../../components/reportingTable";
 
 export default function ReportsView(){
@@ -10,6 +11,9 @@ export default function ReportsView(){
             </Paper>
             <Paper elevation={5} sx={{height: '100%', flex:1}}>
                 <DemoReportingTable/>
+            </Paper>
+            <Paper elevation={5} sx={{height: '100%', maxWidth: "300px"}}>
+                <MemberSidebar/>
             </Paper>
         </Box>
     )
